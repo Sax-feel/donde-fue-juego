7365c62c0d116219e6f4c6fc7084cc777fe7b767
Sax-feel <chingalait@gmail.com>
2026-01-06 12:05:09 -0400

    reglas

--- src/data/gameData.js
+++ src/data/gameData.js
@@ -2,19 +2,18 @@ const gameData = {
     title: "¿Dónde fue?",
     correctAnswer: "Parque La Florida",
     maxAttempts: 5,
-    imageUrl: "./images/florida.jpeg", // Puedes reemplazar con tu imagen
+    imageUrl: "/images/florida.jpeg", // Puedes reemplazar con tu imagen
     clues: [
         "Es un lugar público",
-        "Se encuentra en una zona urbana",
+        "Se encuentra en una zona lejana (De ti jajja)",
         "Tiene áreas verdes y senderos para caminar",
         "Tiene juegos infantiles",
-        "Hay espacios para hacer picnic",
         "Es frecuentado por familias los fines de semana",
-        "Especial: Fuimos caminando",
+        "Especial: Fuimos caminando horas",
         "Especial: Ese día hizo mucho sol",
-        "Especial: Pobres Flores",
+        "Especial: Pobres Flores :´D",
         "Especial: LLegamos justo cuando cerró :´(",
-        "Ultima: 50% de todo :3",
+        "Ultima: 50% :3",
     ]
 };
 
